f6f382fdd24d98ae713f934b6aa5dfa4beebd292
Unknown <bourgeois.thibault599@gmail.com>
2018-11-01 21:57:10 +0100

    chapters persistence

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Chapter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,80 +15,42 @@ class MainController extends AbstractController
      */
     public function index(Request $request)
     {
+        $em = $this->getDoctrine()->getManager();
+        $chapterRepository = $em->getRepository(Chapter::class);
         //dump($request->get('folder'));
         $f = $request->get('folder');
         $b = $request->get('book');
         $pdfFilePath = $this->getParameter('kernel.project_dir') ."/pdf/" .$f ."/" .$b;
-        dump($pdfFilePath);
-        $PDFParser = new Parser();
-        $pdf = $PDFParser->parseFile($pdfFilePath);
-        //$text = nl2br($pdf->getText());
-        $text = $pdf->getText();
-        //dump($text);    //affiche le texte brut provenant du pdf
 
-        //echo str_replace(array("\t"), '', $text);
-        //var_dump(str_replace(array("\t"), '', nl2br($pdf->getText())));
-
-        //dump(str_replace(array("\t"), '', $text));
-
-        $formatedText = preg_split("/[\n]+/", str_replace(array("\t"), '', $text));
-
-        //dump($formatedText);  //affiche le texte brut extrait du pdf
+        $formatedText = $this->pdfToFormatedText(new Parser(), $pdfFilePath);
 
         /*
          * SPLITTING CHAPTERS
          */
-        $nbrChapter = 1;
-        $fullChapters = array();
-        $buffer = '';
-
-        $patternNumChapter = "/^[0-9]+[\s]$/";      //commence par des chiffres et se terminant par un espace
-        $patternNumChapter1 = "/[0-9]+[\s]+$/";      //termine pas des chiffres et un espace
-        $patternNumChapter2= "/[0-9]+$/";           //termine par des chiffres uniquement
-        $patternNumChapter3 = "/[\s]*[0-9]+.?[\s]*$/"; //commence par plusieurs espaces, contient plusieurs chiffres collés puis n caractères et termine par n espaces
-        foreach ($formatedText as $line) {
-
-            if ((preg_match($patternNumChapter3, $line, $matches) || (preg_match($patternNumChapter2, $line, $matches)))) {
-                if(trim($line) == $nbrChapter) {
-                    array_push($fullChapters, $buffer);
-                    $buffer = '';
-                    $nbrChapter ++;
-                }
-            }
-            $buffer = $buffer ." " .trim($line);
-
-        }
-        array_push($fullChapters, $buffer); //add last chapter
-        //dump($fullChapters);    //affiche tout les chapitres triés
-
+        $fullChapters = $this->chaptersSplitterFromFullFormatedText($formatedText);
+        //dump($fullChapters);
 
         /*
          * PARSING CHILDREN INSIDE CHAPTERS
          */
         foreach ($fullChapters as $chapter) {
-            dump($chapter);     //affiche chapitre par chapitre
-            $res = array();
-            //pattern numérique seulement
-            $patternChildChapter = "/[0-9]+/";
-            if(preg_match_all($patternChildChapter, $chapter, $matchedChildren)) {
-                //dump($matchedChildren);
-                if(count($matchedChildren) >= 2) {
-                    //dump($matchedChildren);
-                }
-            }
-            //pattern "au " + numérique
-            $patternChildChapter2 = "/au[\s][0-9]+/";
-            if(preg_match_all($patternChildChapter2, $chapter, $matchedChildren)) {
-                //dump($matchedChildren);
-                foreach ($matchedChildren[0] as $child) {
-                    $exploded = explode(' ', $child);
-                    array_push($res, $exploded[1]);
-                }
-            }
-            dump($res);
-        }
+            $keys = array_keys($fullChapters, $chapter);
+            dump($chapter);
+            if($chapterRepository->findOneBy(array("numChapter" => $keys[0])) === null) {
+                $newChapter = new Chapter();
+                $newChapter->setNumChapter($keys[0]);
+                $newChapter->setText($chapter);
 
-        //dump($fullChapters);
+                $em->persist($newChapter);
+                $em->flush();
+            }
+
+            dump($chapterRepository->findOneBy(array("numChapter" => $keys[0])));
+
+            $res = $this->parsingChildrenInsideChapter($chapter);
+
+            //dump($res);
+        }
 
         return $this->json([
             'message' => 'Welcome to your new controller!',
@@ -124,51 +87,16 @@ class MainController extends AbstractController
         foreach ($listBooks as $book) {
             //dump($this->getParameter('kernel.project_dir') ."/pdf/test/" .trim($book));
             $pdfFilePath = $this->getParameter('kernel.project_dir') ."/pdf/" .$f ."/" .trim($book);
-            //dump($pdfFilePath);
-            $PDFParser = new Parser();
-            try {
-                $pdf = $PDFParser->parseFile($pdfFilePath);
-            } catch (\Exception $exception) {
-                array_push($listBookProtected, $book);
-                $cptProtected ++;
-            }
-
-            //$text = nl2br($pdf->getText());
-            $text = $pdf->getText();
-            //dump($text);
-
-            //echo str_replace(array("\t"), '', $text);
-            //var_dump(str_replace(array("\t"), '', nl2br($pdf->getText())));
-
-            //dump(str_replace(array("\t"), '', $text));
-
-            $formatedText = preg_split("/[\n]+/", str_replace(array("\t"), '', $text));
-
-            //dump($formatedText);  //affiche le texte brut extrait du pdf
+            $formatedText = $this->pdfToFormatedText(new Parser(),$pdfFilePath);
 
             /*
              * SPLITTING CHAPTERS
              */
-            $nbrChapter = 1;
-            $fullChapters = array();
-            $buffer = '';
+            $fullChapters = $this->chaptersSplitterFromFullFormatedText($formatedText);
 
-            $patternNumChapter = "/^[0-9]+[\s]$/";      //commence par des chiffres et se terminant par un espace
-            $patternNumChapter1 = "/[0-9]+[\s]+$/";      //contient des chiffres et termine par un espace
-            $patternNumChapter2= "/[0-9]+$/";      //termine par des chiffres uniquement
-            $patternNumChapter3 = "/[\s]*[0-9]+.*[\s]*$/"; //commence par plusieurs espaces, contient plusieurs chiffres collés puis n caractères et termine par n espaces
-            foreach ($formatedText as $line) {
-                if ((preg_match($patternNumChapter3, $line, $matches) || (preg_match($patternNumChapter2, $line, $matches)))) {
-                    if(trim($line) == $nbrChapter) {
-                        array_push($fullChapters, $buffer);
-                        $buffer = '';
-                        $nbrChapter ++;
-                    }
-                }
-                $buffer = $buffer ." " .trim($line);
-
-            }
-            array_push($fullChapters, $buffer); //add last chapter
+            /*
+             * TEST VALIDITE EMPIRIQUE (si il existe au moins 250 chapitres)
+             */
 
             if(isset($fullChapters[220])) {
                 array_push($listBookOk, $book);
@@ -199,4 +127,78 @@ class MainController extends AbstractController
     }
 
 
+    public function pdfToFormatedText(Parser $PDFParser, $pdfFilePath) {
+        //$PDFParser = new Parser();
+        try {
+            $pdf = $PDFParser->parseFile($pdfFilePath);
+            $text = $pdf->getText();
+            //dump($text);    //affiche le texte brut provenant du pdf
+
+            $formatedText = preg_split("/[\n]+/", str_replace(array("\t"), '', $text));
+            //dump($formatedText);  //affiche le texte brut extrait du pdf
+
+            return $formatedText;
+        } catch (\Exception $exception) {
+            //array_push($listBookProtected, $book);
+            //$cptProtected ++;
+            return false;
+        }
+    }
+
+    public function chaptersSplitterFromFullFormatedText($formatedText) {
+        $nbrChapter = 1;
+        $fullChapters = array();
+        $buffer = '';
+
+        //dump($formatedText);
+
+        $patternNumChapter = "/^[0-9]+[\s]$/";      //commence par des chiffres et se terminant par un espace
+        $patternNumChapter1 = "/[0-9]+[\s]+$/";      //termine pas des chiffres et un espace
+        $patternNumChapter2= "/[0-9]+$/";           //termine par des chiffres uniquement
+        $patternNumChapter3 = "/[\s]*[0-9]+.?[\s]*$/"; //commence par plusieurs espaces, contient plusieurs chiffres collés puis n caractères et termine par n espaces
+        foreach ($formatedText as $line) {
+
+            if ((preg_match($patternNumChapter3, $line, $matches) || (preg_match($patternNumChapter2, $line, $matches)))) {
+                if(trim($line) == $nbrChapter) {
+                    $patternText = "/^[\s][0-9]{1,2}[\s]/";
+                    $formattedText = preg_replace($patternText, "", $buffer);
+                    array_push($fullChapters, $formattedText);
+                    $buffer = '';
+                    $nbrChapter ++;
+                }
+            }
+            $buffer = $buffer ." " .trim($line);
+
+        }
+        array_push($fullChapters, $buffer); //add last chapter
+        //dump($fullChapters);    //affiche tout les chapitres triés
+
+        return $fullChapters;
+    }
+
+    public function parsingChildrenInsideChapter($chapter) {
+
+        //dump($chapter);     //affiche chapitre par chapitre
+        $childrenArray = array();
+        //pattern numérique seulement
+        $patternChildChapter = "/[0-9]+/";
+        if(preg_match_all($patternChildChapter, $chapter, $matchedChildren)) {
+            //dump($matchedChildren);
+            if(count($matchedChildren) >= 2) {
+                //dump($matchedChildren);
+            }
+        }
+        //pattern "au " + numérique
+        $patternChildChapter2 = "/au[\s][0-9]+/";
+        if(preg_match_all($patternChildChapter2, $chapter, $matchedChildren)) {
+            //dump($matchedChildren);
+            foreach ($matchedChildren[0] as $child) {
+                $exploded = explode(' ', $child);
+                array_push($childrenArray, $exploded[1]);
+            }
+        }
+        //dump($childrenArray);  //return all children for this chapter
+
+        return $childrenArray;
+    }
 }
---
+++ src/Entity/Chapter.php
@@ -0,0 +1,150 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ChapterRepository")
+ */
+class Chapter
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $numChapter;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $text;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="Chapter", cascade={"persist", "remove"}, inversedBy="children", fetch="EAGER")
+     * @ORM\JoinTable(name="pathing_chapters")
+     */
+    private $parents;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="Chapter", mappedBy="parent", fetch="EAGER")
+     */
+    private $children;
+
+
+    //--------------------------
+
+
+    /**
+     * Chapter constructor.
+     */
+    public function __construct()
+    {
+        $this->parents = new ArrayCollection();
+        $this->children = new ArrayCollection();
+    }
+
+
+    //--------------------------
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNumChapter()
+    {
+        return $this->numChapter;
+    }
+
+    /**
+     * @param mixed $numChapter
+     */
+    public function setNumChapter($numChapter): void
+    {
+        $this->numChapter = $numChapter;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getText()
+    {
+        return $this->text;
+    }
+
+    /**
+     * @param mixed $text
+     */
+    public function setText($text): void
+    {
+        $this->text = $text;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getParents()
+    {
+        return $this->parents;
+    }
+
+    /**
+     * @param mixed $parents
+     */
+    public function setParents($parents): void
+    {
+        $this->parents = $parents;
+    }
+
+    /**
+     * @param mixed $parent
+     */
+    public function addParent($parent): void
+    {
+        /** @var  $parentsList ArrayCollection*/
+        $parentsList = $this->getParents();
+        $parentsList->add($parent);
+        $this->parents = $parentsList;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getChildren()
+    {
+        return $this->children;
+    }
+
+    /**
+     * @param mixed $children
+     */
+    public function setChildren($children): void
+    {
+        $this->children = $children;
+    }
+
+    /**
+     * @param mixed $child
+     */
+    public function addChild($child): void
+    {
+        /** @var  $childrenList ArrayCollection*/
+        $childrenList = $this->getChildren();
+        $childrenList->add($child);
+        $this->children = $childrenList;
+    }
+
+
+}
